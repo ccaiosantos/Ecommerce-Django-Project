9f172071d455afa685a09ff2a3c7198ffb4fe0e2
ccaiosantos <c.caiogsantos@gmail.com>
2026-05-15 20:00:22 -0300

    Models na pasta produto contendo os campos de produto no banco de dados, e tambem criei a classe variação dentro dos models de produto para ter mais de uma opçao de produto, por exemplo: um iphone de 64GB e um iphone de 128GB, isso é uma variação, e tambem coloquei no admin

--- loja/settings.py
+++ loja/settings.py
@@ -10,9 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 import os
+from django.conf import settings
 from django.contrib.messages import constants
 from pathlib import Path
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'produto',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
